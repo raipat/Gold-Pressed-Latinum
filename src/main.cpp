670f9febc82aef395eb09721f25fce2604ef8568
scificrypto <realgrandnagus@gmail.com>
2016-09-20 15:18:11 -0400

    Revert GetBlockTrust() calculation.
    New GetBlockTrust() reduce PoS speed more then 10 times and was reverted
    to standart.

--- src/main.cpp
+++ src/main.cpp
@@ -2155,7 +2155,7 @@ bool CBlock::AcceptBlock()
 
     return true;
 }
-
+/*
 CBigNum CBlockIndex::GetBlockTrust() const
 {
     CBigNum bnTarget;
@@ -2191,7 +2191,7 @@ CBigNum CBlockIndex::GetBlockTrust() const
     return (IsProofOfStake()? (CBigNum(1)<<256) / (bnTarget+1) : 1);
 }
  
-
+*/
 bool CBlockIndex::IsSuperMajority(int minVersion, const CBlockIndex* pstart, unsigned int nRequired, unsigned int nToCheck)
 {
     unsigned int nFound = 0;
@@ -3995,7 +3995,7 @@ CBlock* CreateNewBlock(CWallet* pwallet, bool fProofOfStake)
         }
     }
 
-    pblock->nBits = GetNextTargetRequired(pindexPrev, fProofOfStake);
+    pblock->nBits = GetNextTargetRequired(pindexPrev, pblock->IsProofOfStake());
 
     // Collect memory pool transactions into the block
     int64 nFees = 0;
@@ -4110,7 +4110,7 @@ CBlock* CreateNewBlock(CWallet* pwallet, bool fProofOfStake)
                 continue;
 
             // Timestamp limit
-            if (tx.nTime > GetAdjustedTime() || (fProofOfStake && tx.nTime > pblock->vtx[1].nTime))
+            if (tx.nTime > GetAdjustedTime() || (pblock->IsProofOfStake() && tx.nTime > pblock->vtx[1].nTime))
                 continue;
 
             // ppcoin: simplify transaction fee - allow free = false
@@ -4189,16 +4189,16 @@ CBlock* CreateNewBlock(CWallet* pwallet, bool fProofOfStake)
         if (fDebug && GetBoolArg("-printpriority"))
             printf("CreateNewBlock(): total size %"PRI64u"\n", nBlockSize);
 
-        if (!fProofOfStake)
+        if (pblock->IsProofOfWork())
             pblock->vtx[0].vout[0].nValue = GetProofOfWorkReward(pblock->nBits);
 
         // Fill in header
         pblock->hashPrevBlock  = pindexPrev->GetBlockHash();
-        if (fProofOfStake)
+        if (pblock->IsProofOfStake())
             pblock->nTime      = pblock->vtx[1].nTime; //same as coinstake timestamp
         pblock->nTime          = max(pindexPrev->GetMedianTimePast()+1, pblock->GetMaxTransactionTime());
         pblock->nTime          = max(pblock->GetBlockTime(), pindexPrev->GetBlockTime() - nMaxClockDrift);
-        if (!fProofOfStake)
+        if (pblock->IsProofOfWork())
             pblock->UpdateTime(pindexPrev);
         pblock->nNonce         = 0;
     }
