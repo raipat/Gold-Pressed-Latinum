bb4240c0c919eed98cf472823532095c592f9cda
scificrypto <realgrandnagus@gmail.com>
2016-09-20 15:20:34 -0400

    Add checkpoint and new version info.

--- src/version.cpp
+++ src/version.cpp
@@ -37,7 +37,7 @@ const std::string CLIENT_NAME("Latinum");
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
 #    define GIT_COMMIT_ID ""
-#    define GIT_COMMIT_DATE "20 March 2016"
+#    define GIT_COMMIT_DATE "20 September 2016"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
